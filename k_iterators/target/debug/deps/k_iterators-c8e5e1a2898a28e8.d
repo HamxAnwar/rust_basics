7bfec9ffe98b8c3a0afa0185aa409ed80ee3ad5e
HamxAnwar <hamzaanwar93@outlook.com>
2024-09-16 18:55:43 +0500

    Iterators completed

--- k_iterators/target/debug/deps/k_iterators-c8e5e1a2898a28e8.d
+++ k_iterators/target/debug/deps/k_iterators-c8e5e1a2898a28e8.d
@@ -1,5 +1,5 @@
-/home/hamza/Projects/rust_basics/k_iterators/target/debug/deps/libk_iterators-c8e5e1a2898a28e8.rmeta: src/main.rs
+/home/robotics/Projects/rust_basics/k_iterators/target/debug/deps/libk_iterators-c8e5e1a2898a28e8.rmeta: src/main.rs
 
-/home/hamza/Projects/rust_basics/k_iterators/target/debug/deps/k_iterators-c8e5e1a2898a28e8.d: src/main.rs
+/home/robotics/Projects/rust_basics/k_iterators/target/debug/deps/k_iterators-c8e5e1a2898a28e8.d: src/main.rs
 
 src/main.rs:
